311ea5358695cd062e130eb31c9b04afc6f5c737
Mercutio <mercutio_john@foxmail.com>
2023-05-26 08:41:11 +0000

    svg ok

--- src/assets/svg-icons/index.js
+++ src/assets/svg-icons/index.js
@@ -1,12 +1,8 @@
-import Vue from 'vue'
-console.log('[IconSvg] import')
+import Vue from "vue";
 
-const requireAll = requireContext => requireContext.keys().map(requireContext)
-const req = require.context('./icons', false, /\.svg$/)
-const iconMap = requireAll(req)
+const requireAll = (requireContext) =>
+  requireContext.keys().map(requireContext);
+const req = require.context("./icons", false, /\.svg$/);
+const iconMap = requireAll(req);
 
-// Vue.prototype.$IconSvg = iconMap.map(e => e.default.id.slice(3))
-Vue.prototype.$IconSvg = iconMap.map(e => {
-    console.log(e)
-    return e.default.id.slice(3)
-})
+Vue.prototype.$IconSvg = iconMap.map((e) => e.default.id.slice(3));
